2b72ad2cfd4419017308b81a76c77b20d9c4d59a
Poojapawar12345678 <129867665+Poojapawar12345678@users.noreply.github.com>
2023-10-03 22:32:19 +0530

    Add files via upload

---
+++ ShopMain (1).java
@@ -0,0 +1,13 @@
+
+public class ShopMain {
+	
+
+	public static void main(String[] args) {
+		
+		ShopInfo sinfo=new ShopInfo();
+		Customer cust=sinfo.create();
+		Bill billobj=sinfo.calculateBill(cust);
+		sinfo.display(cust, billobj);
+		
+	}
+}
